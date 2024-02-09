2584c6d6941870a2233b97ee89f28bf25af91f77
CarterTram <136946140+CarterTram@users.noreply.github.com>
2024-02-09 10:46:50 -0500

    Add files via upload

---
+++ SkiSlope/src/SkiSlopeTest.java
@@ -0,0 +1,58 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class SkiSlopeTest {
+
+    @Test
+    public void testCountRuns() {
+        // Test case 1, this is the 3 10
+    	//1 2 1 2 1 3 2 3 2 4 2 4 4 0 4 0 3 0 3 0
+
+        int numNodes1 = 3;
+        int[][] multiplier1 = {
+            {0, 0, 0, 0, 0},
+            {0, 0, 2, 1, 0},
+            {0, 0, 0, 1, 2},
+            {2, 0, 0, 0, 0},
+            {2, 0, 0, 0, 0}
+        };
+        assertEquals(14, SkiSlope.countRuns(numNodes1 + 2, multiplier1));
+
+        // Test case 2
+        int numNodes2 = 2;
+        int[][] multiplier2 = {
+            {0, 0, 0, 0},
+            {0, 1, 2, 0},
+            {1, 0, 0, 0},
+            {1, 0, 0, 0}
+        };
+        assertEquals(2, SkiSlope.countRuns(numNodes2 + 2, multiplier2));
+
+        // Test case 3
+        int numNodes3 = 2;
+        int[][] multiplier3 = {
+            {0, 0, 0, 0},
+            {0, 0, 3, 1},
+            {2, 0, 0, 1},
+            {0, 0, 0, 0}
+        };
+        assertEquals(6, SkiSlope.countRuns(numNodes3 + 2, multiplier3));
+
+        // Test case 4: No nodes connected
+        int numNodes4 = 2;
+        int[][] multiplier4 = {
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+        };
+        assertEquals(0, SkiSlope.countRuns(numNodes4 + 2, multiplier4));
+        //not exactly ever going to happen, but if numNodes = 0 it still works
+        int numNodes5 = 0; 
+        int[][] multiplier5 = {
+            {0, 0},
+            {0, 0}
+        };
+        assertEquals(0, SkiSlope.countRuns(numNodes5 + 2, multiplier5));
+    }
+}
